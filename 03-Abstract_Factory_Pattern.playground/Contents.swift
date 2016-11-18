2fa9dc5971886d7fcad802fcb6a397a678c96dc5
kingcos <2821836721v@gmail.com>
2016-11-18 23:49:00 +0800

    Abstract Factory Pattern

---
+++ 03-Abstract_Factory_Pattern.playground/Contents.swift
@@ -0,0 +1,57 @@
+//: Playground - noun: a place where people can play
+// Powered by https://maimieng.com from https://github.com/kingcos/Swift-3-Design-Patterns
+
+import UIKit
+
+// 协议
+protocol Operator {
+    // 工厂
+    static func calculate() -> Calculator
+}
+
+// 协议
+protocol Calculator {
+    var num: (Double, Double) { get set }
+    
+    func getResult() -> Double?
+}
+
+struct Addition: Operator {
+    // 工厂
+    static func calculate() -> Calculator {
+        return Add()
+    }
+}
+
+struct Subtraction: Operator {
+    // 工厂
+    static func calculate() -> Calculator {
+        return Subtract()
+    }
+}
+
+// 遵守协议
+struct Add: Calculator {
+    internal var num = (0.0, 0.0)
+    
+    internal func getResult() -> Double? {
+        return num.0 + num.1
+    }
+}
+
+// 遵守协议
+struct Subtract: Calculator {
+    internal var num = (0.0, 0.0)
+    
+    internal func getResult() -> Double? {
+        return num.0 - num.1
+    }
+}
+
+var factory = Addition.calculate()
+factory.num = (1, 1)
+print(factory.getResult() ?? "Error")
+
+factory = Subtraction.calculate()
+factory.num = (1, 1)
+print(factory.getResult() ?? "Error")
