aea569321dd2359d44503c3f1a8c6d096063fb7b
kingcos <2821836721v@gmail.com>
2016-12-13 23:53:31 +0800

    Some code added

--- 24-Interpreter_Pattern.playground/Contents.swift
+++ 24-Interpreter_Pattern.playground/Contents.swift
@@ -75,8 +75,25 @@ class Scale: Expression {
     }
 }
 
+// 音速
+class Speed: Expression {
+    override func execute(_ key: String, value: Double) {
+        var speed: String
+        
+        if value >= 1000 {
+            speed = "高速"
+        } else if value < 500 {
+            speed = "低速"
+        } else {
+            speed = "中速"
+        }
+        
+        print("\(speed)", separator: "", terminator: " ")
+    }
+}
+
 var context = PlayContext(text:
-    " O 2 E 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0.5 O 3 C 1 O 2 A 0.5 G 1 C 0.5 E 0.5 D 3 "
+    " T 400 O 2 E 0.5 A 3 E 0.5 G 0.5 D 3 E 0.5 G 0.5 A 0.5 O 3 C 1 O 2 A 0.5 G 1 C 0.5 E 0.5 D 3 "
 )
 
 while context.text.characters.count > 1 {
@@ -86,6 +103,8 @@ while context.text.characters.count > 1 {
     var expression = Expression()
     
     switch str {
+    case "T":
+        expression = Speed()
     case "O":
         expression = Scale()
     case "C", "D", "E", "F", "G", "A", "B", "P":
