eac44accfde418956a111bd6c907730ebab5b82d
kingcos <2821836721v@gmail.com>
2021-04-13 21:08:57 +0800

    Add template method

---
+++ 08-Template_Pattern.playground/Sources/TemplateMethod.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+class AbstractDisplay { // Abstract Class
+    func open() { fatalError() }
+    func print() { fatalError() }
+    func close() { fatalError() }
+    func display() {
+        open()
+        
+        for _ in 0..<5 {
+            print()
+        }
+        
+        close()
+    }
+}
+
+class CharDisplay: AbstractDisplay { // ConcreteClass
+    var char: Character
+    
+    init(_ char: Character) {
+        self.char = char
+    }
+    
+    override func open() {
+        Swift.print("<<", separator: "", terminator: "")
+    }
+    
+    override func print() {
+        Swift.print(char, separator: "", terminator: "")
+    }
+    
+    override func close() {
+        Swift.print(">>")
+    }
+}
+
+class StringDisplay: AbstractDisplay { // ConcreteClass
+    var string: String
+    var width: Int
+    
+    init(_ string: String) {
+        self.string = string
+        self.width = string.count
+    }
+    
+    override func open() {
+        printLine()
+    }
+    
+    override func print() {
+        Swift.print("|" + string + "|")
+    }
+    
+    override func close() {
+        printLine()
+    }
+    
+    func printLine() {
+        Swift.print("+", separator: "", terminator: "")
+        for _ in 0..<width {
+            Swift.print("-", separator: "", terminator: "")
+        }
+        Swift.print("+")
+    }
+}
+
+public func testTemplateMethod() {
+    let dp1 = CharDisplay("V")
+    let dp2 = StringDisplay("kingcos.me")
+    let dp3 = StringDisplay("萌面大道")
+    
+    dp1.display()
+    dp2.display()
+    dp3.display()
+}
---
+++ 08-Template_Pattern.playground/Sources/TemplatePattern.swift
@@ -0,0 +1,84 @@
+import Foundation
+
+// 考试协议
+protocol Exam {
+    func questionA()
+    func questionB()
+    func questionC()
+    
+    func answerA() -> String
+    func answerB() -> String
+    func answerC() -> String
+}
+
+// 试卷
+class TestPaper: Exam {
+    // 模版方法
+    func questionA() {
+        print("Q&A 1 \(answerA())")
+    }
+    
+    func questionB() {
+        print("Q&A 2 \(answerB())")
+    }
+    
+    func questionC() {
+        print("Q&A 3 \(answerC())")
+    }
+    
+    func answerA() -> String {
+        return ""
+    }
+    
+    func answerB() -> String {
+        return ""
+    }
+    
+    func answerC() -> String {
+        return ""
+    }
+}
+
+// A 的试卷
+class TestPaperA: TestPaper {
+    override func answerA() -> String {
+        return "A"
+    }
+    
+    override func answerB() -> String {
+        return "B"
+    }
+    
+    override func answerC() -> String {
+        return "C"
+    }
+}
+
+// B 的试卷
+class TestPaperB: TestPaper {
+    override func answerA() -> String {
+        return "C"
+    }
+    
+    override func answerB() -> String {
+        return "B"
+    }
+    
+    override func answerC() -> String {
+        return "A"
+    }
+}
+
+public func testTemplatePattern() {
+    let pA = TestPaperA()
+
+    pA.questionA()
+    pA.questionB()
+    pA.questionC()
+
+    let pB = TestPaperB()
+
+    pB.questionA()
+    pB.questionB()
+    pB.questionC()
+}
